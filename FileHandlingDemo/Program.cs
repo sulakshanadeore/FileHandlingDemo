a9a8502075ef254b52b74d52671e29dff3972042
deoresulakshana <deoresulakshana@gmail.com>
2023-10-19 12:25:03 +0530

    Add project files.

---
+++ FileHandlingDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace FileHandlingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Create("firstFile.txt");
+            //string[] s = new string[2] {"Hello","World" };
+            //File.WriteAllLines("secondfile.txt", s);
+            //File.WriteAllText("D:\\NewDrive\\MphasisSept\\abc.txt", "Welcome...File Trial ");
+            //File.WriteAllLines(@"D:\NewDrive\MphasisSept\xyz.txt", s);
+            //File.Create(@"D:\NewDrive\MphasisSept\pqe.txt");
+            //File.Delete(@"D:\NewDrive\MphasisSept\pqe.txt");
+            //File.Delete(@"D:\NewDrive\MphasisSept\abc.txt");
+            //File.Delete(@"D:\NewDrive\MphasisSept\xyz.txt");
+
+            //Directory.CreateDirectory("dir1");
+            //File.Move("secondfile.txt", "dir1\\secfile1.txt");
+            //File.Move("D:\\NewDrive\\MphasisSept\\gcNotes.txt", "dir1\\mygarbageCollectionNotes.txt");
+            //File.Copy("firstFile.txt", "dir1\\firstFile.txt");
+            //string s=File.ReadAllText("firstFile.txt");
+            //File.AppendAllText(@"dir1\\secfile1.txt", s);
+            IEnumerable<string> files=Directory.EnumerateFiles("dir1");
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("--------------------");
+            IEnumerable<string> dirs=Directory.EnumerateDirectories("D:\\NewDrive\\MphasisSept\\");
+            foreach (var item in dirs)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(  "------------------------------");
+            string[]  filesinDir=Directory.GetFiles(@"D:\NewDrive\MphasisSept\");
+            foreach (var item in filesinDir)
+            {
+                Console.WriteLine(  item);
+            }
+
+
+
+
+
+
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ FileHandlingDemo/WorkingWithTextFiles.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace FileHandlingDemo
+{
+    internal class WorkingWithTextFiles
+    {
+        static void Main(string[] args)
+        {
+            //createFileandWrite();
+            //bool ans=File.Exists(@"D:\\NewDrive\\MphasisSept\\file3.txt");
+            //if (ans) {
+                FileStream fs = null;
+                try
+                {
+
+                    fs = new FileStream(@"D:\\NewDrive\\MphasisSept\\file2.txt", FileMode.Open, FileAccess.Read);
+                    StreamReader sr = null;
+                    try
+                    {
+                        sr = new StreamReader(fs);
+                        string data = sr.ReadToEnd();
+                        Console.WriteLine(data);
+
+                    }
+                    catch (Exception ex)
+                    {
+
+                        Console.WriteLine(ex.Message);
+
+                    }
+
+                    finally
+                    {
+
+                        sr.Close();
+                        sr.Dispose();
+                        fs.Close();
+                        fs.Dispose();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            //else
+            //{
+              //  Console.WriteLine(  "File doesn't exists");
+
+            //}
+            
+           
+             
+                Console.Read();
+
+
+
+
+
+        }
+
+        private static void createFileandWrite()
+        {
+            FileStream fs = new FileStream(@"D:\NewDrive\MphasisSept\file2.txt", FileMode.Create, FileAccess.Write);
+            //streamwriter represent pen/pencil
+            StreamWriter writer = new StreamWriter(fs);
+            try
+            {
+                //pen/pencil----same line(Write), newline(WriteLine)
+                writer.Write("Hello World");
+                writer.Write("This is second line in the file");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                writer.Flush();//flush means the buffer data if anything is remaining will be written to the file
+                writer.Close();
+                writer.Dispose();
+                fs.Close();//close and save
+                fs.Dispose();
+
+            }
+        }
+    }
+}
